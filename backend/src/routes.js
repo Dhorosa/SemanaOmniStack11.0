d4e65979f9b62ce0cc02a48be2db80b1cafb9712
DaniellOlliva <53453696+Dhorosa@users.noreply.github.com>
2020-07-09 20:44:02 -0300

    Validação de Incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -22,13 +22,19 @@ routes.post('/ongs', celebrate({
      [Segments.BODY]:Joi.object().keys({
         name:Joi.string().required(),
         email: Joi.string().required().email(),
-        whatsapp: Joi.number().required().min(10).max(11),
+        whatsapp: Joi.string().required().min(10).max(11),
         city: Joi.string().required(),
         uf: Joi.string().required().length(2),
     })
 }) , ongcontroller.create);
 
-routes.post('/incidents', incidentscontroller.create);
+routes.post('/incidents',celebrate({
+    [Segments.BODY]:Joi.object().keys({
+        title:Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),  
+    }) 
+}), incidentscontroller.create);
 
 routes.post('/session', sessioncontroller.create);
 
